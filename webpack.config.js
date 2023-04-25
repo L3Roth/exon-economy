3b4877d0ee9d8a2f70dddd4531686d8b10e81f4e
Alexander <alexander.noack95@outlook.de>
2023-04-25 22:24:26 +0200

    - found and resolved path error of webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,26 @@
+import * as path from 'path';
+
 module.exports = {
-    resolve: {
-        fallback: {path: require.resolve("path-browserify")}
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "fs": false,
+      "os": false,
+      "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "zlib": require.resolve("browserify-zlib"),
+      "util": require.resolve("util/")
+    },
+    alias: {
+      'path': path.resolve(__dirname, 'node_modules/path-browserify'),
+      'jasmine-core': path.resolve(__dirname, 'node_modules/jasmine-core/lib/jasmine-core/jasmine.js')
+
+    },
+    node: {
+        global: true,
+        __filename: true,
+        __dirname: true,
     }
-}
+  }
+};
